ef362a1793e41f4deb185b49a3e027c04b37392c
Steven Mpawulo <113428438+steven-mpawulo@users.noreply.github.com>
2023-05-24 20:27:19 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, jsonify
 import werkzeug
 import deepspeech
-import numpy as np
 import soundfile as sf
 import librosa
 import re
@@ -10,150 +9,101 @@ import os
 
 app = Flask(__name__)
 
+# Load the DeepSpeech model
+model_path = 'resources/deepspeech-0.9.3-models.tflite'
+model = deepspeech.Model(model_path)
+
 @app.route('/', methods=['GET'])
 def home():
-  return jsonify({'result': 'Welcome'})
+    return jsonify({'result': 'Welcome'})
 
 @app.route('/check_pronunciation', methods=['POST'])
 def check_pronunciation():
-    # Load the DeepSpeech model
-    model_path = 'resources/deepspeech-0.9.3-models.tflite'
-    model = deepspeech.Model(model_path)
+    try:
+        # Get the audio data from the request
+        if 'file' not in request.files:
+            return jsonify({'code': 400, 'message': 'No file attached'}), 400
+        file = request.files['file']
 
-    # Get the audio data from the request
-    if 'file' not in request.files:
-        return jsonify({'code': 400, 'message': 'No file attached'}), 400
-    file = request.files['file']
+        if file.content_type.split('/')[0] != 'audio':
+            return jsonify({'code': 400, 'message': 'Invalid file type'}), 400
 
-    if file.content_type.split('/')[0] != 'audio':
-        return jsonify({'code': 400, 'message': 'Invalid file type'}), 400
+        # Save the audio file
+        directory = "assets"
+        if not os.path.exists(directory):
+            os.makedirs(directory)
 
-    fileName = werkzeug.utils.secure_filename(file.filename)
-    directory = "assets"
-    if not os.path.exists(directory):
-      os.makedirs(directory)
-    file.save("assets/" + fileName)
-    # audio_data = request.files['audio'].read()
+        file_path = os.path.join(directory, werkzeug.utils.secure_filename(file.filename))
+        file.save(file_path)
 
-    # Load the audio data
-    # audio_data, sample_rate = sf.read(str("assets/" + fileName))
-    audio_data, sample_rate = librosa.load(str("assets/" + fileName))
+        # Load the audio data
+        audio_data, sample_rate = librosa.load(file_path)
 
+        # Transcribe the audio using DeepSpeech
+        text = model.stt(audio_data)
 
-    # Convert the audio data to the correct format
-    if audio_data.dtype != 'int16':
-        audio_data = np.int16(audio_data * (2 ** 15 - 1))
+        # Preprocess the text
+        text = re.sub('[^a-zA-Z ]+', '', text.lower())
 
-    # Get the word from the request
-    word = request.form['word']
+        # Get the word from the request
+        word = request.form['word']
 
-    # Transcribe the audio using DeepSpeech
-    text = model.stt(audio_data)
+        # Define the phonemes
+        phonemes = {
+            'a': 'a',
+            'b': 'b',
+            'ch': 'ʧ',
+            'd': 'd',
+            'dh': 'ð',
+            'e': 'e',
+            'f': 'f',
+            'g': 'ɡ',
+            'gh': 'ɣ',
+            'h': 'h',
+            'i': 'i',
+            'j': 'dʒ',
+            'k': 'k',
+            'kh': 'x',
+            'l': 'l',
+            'm': 'm',
+            'n': 'n',
+            'ng': 'ŋ',
+            'ny': 'ɲ',
+            'o': 'o',
+            'p': 'p',
+            'r': 'ɾ',
+            's': 's',
+            'sh': 'ʃ',
+            't': 't',
+            'th': 'θ',
+            'u': 'u',
+            'v': 'v',
+            'w': 'w',
+            'y': 'j',
+            'z': 'z',
+            'zh': 'ʒ'
+        }
 
-    # Preprocess the text
-    text = re.sub('[^a-zA-Z ]+', '', text.lower())
+        # Convert the text to phonemes
+        phoneme_text = ''.join([phonemes.get(letter, letter) for letter in text])
 
-    # Define the Kiswahili phonemes
-    # phonemes = {
-    #     'a': 'ɑ',
-    #     'b': 'b',
-    #     'd': 'd',
-    #     'e': 'ɛ',
-    #     'f': 'f',
-    #     'g': 'ɡ',
-    #     'h': 'h',
-    #     'i': 'i',
-    #     'j': 'dʒ',
-    #     'k': 'k',
-    #     'l': 'l',
-    #     'm': 'm',
-    #     'n': 'n',
-    #     'o': 'ɔ',
-    #     'p': 'p',
-    #     'r': 'r',
-    #     's': 's',
-    #     't': 't',
-    #     'u': 'u',
-    #     'v': 'v',
-    #     'w': 'w',
-    #     'y': 'j',
-    #     'z': 'z',
-    #     'ng': 'ŋ',
-    #     'ny': 'ɲ',
-    #     'sh': 'ʃ',
-    #     'ch': 'tʃ',
-    #     'kh': 'x',
-    #     'gh': 'ɣ',
-    #     'ph': 'pʰ',
-    #     'th': 'tʰ',
-    # }
-    phonemes = {
-    'a': 'a',
-    'b': 'b',
-    'ch': 'ʧ',
-    'd': 'd',
-    'dh': 'ð',
-    'e': 'e',
-    'f': 'f',
-    'g': 'ɡ',
-    'gh': 'ɣ',
-    'h': 'h',
-    'i': 'i',
-    'j': 'dʒ',
-    'k': 'k',
-    'kh': 'x',
-    'l': 'l',
-    'm': 'm',
-    'n': 'n',
-    'ng': 'ŋ',
-    'ny': 'ɲ',
-    'o': 'o',
-    'p': 'p',
-    'r': 'ɾ',
-    's': 's',
-    'sh': 'ʃ',
-    't': 't',
-    'th': 'θ',
-    'u': 'u',
-    'v': 'v',
-    'w': 'w',
-    'y': 'j',
-    'z': 'z',
-    'zh': 'ʒ'
-}
+        # Convert the word to phonemes
+        phoneme_word = ''.join([phonemes.get(letter, letter) for letter in word])
 
+        # Calculate the Levenshtein distance between the two sequences
+        distance = Levenshtein.distance(phoneme_text, phoneme_word)
 
-    # Convert the text to phonemes
-    phoneme_text = ''
-    for letter in text:
-        if letter == ' ':
-            phoneme_text += ' '
-        elif letter in phonemes:
-            phoneme_text += phonemes[letter]
+        # Define a threshold for the distance
+        threshold = 3
+
+        # Return True if the distance is below the threshold, indicating correct pronunciation
+        if distance <= threshold:
+            return jsonify({'result': True, 'phoneme_text': phoneme_text, 'phoneme_word': phoneme_word})
         else:
-            phoneme_text += letter
+            return jsonify({'result': False, 'phoneme_text': phoneme_text, 'phoneme_word': phoneme_word})
 
-    # Convert the word to phonemes
-    phoneme_word = ''
-    for letter in word:
-        if letter == ' ':
-            phoneme_word += ' '
-        elif letter in phonemes:
-            phoneme_word += phonemes[letter]
-        else:
-            phoneme_word += letter
-
-    # Calculate the Levenshtein distance between the two sequences
-    distance = Levenshtein.distance(phoneme_text, phoneme_word)
-
-    # Define a threshold for the distance
-    threshold = 3
-
-    # Return True if the distance is below the threshold, indicating correct pronunciation
-    if distance <= threshold:
-        return jsonify({'result': True, 'phoneme_text': phoneme_text, 'phoneme_word': phoneme_word})
-    else:
-        return jsonify({'result': False, 'phoneme_text': phoneme_text, 'phoneme_word': phoneme_word})
+    except Exception as e:
+        return jsonify({'code': 500, 'message': 'An error occurred', 'error': str(e)}), 500
 
 if __name__ == '__main__':
     app.run(debug=False)
